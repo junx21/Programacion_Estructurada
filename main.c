ebc225bea2773f32b80b4ac3cf13db9046944c52
junx21 <jxuche@myuax.com>
2024-11-21 18:05:03 +0100

    main

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "EJ1/estructuras.h"
+#include "EJ2/uniones.h"
+#include "EJ3/copia_estructuras.h"
+#include "EJ4/alias.h"
+#include "EJ5/estructuras_funciones.h"
+
+int main() {
+
+    estructuras();
+    uniones();
+    alias();
+    estructura_funciones();
+
+
+    return 0;
+}
